ce959a859255c3b919b8c9016ebc4cc6d8fa25b0
newton2ndlaw <1039255811@qq.com>
2016-03-11 14:20:00 +0800

    Update Homework3-2.py

--- Homework3-2.py
+++ Homework3-2.py
@@ -1,6 +1,3 @@
-import pygame
-pygame.init()
-
 #letter list designed by Zhang Qi
 letter=[['     ','     ','     ','     ','     ']] #space
 letter.append(['  #  ',' # # ','#####','#   #','#   #']) #A 1
